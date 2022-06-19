638ded5339fa06f661f67dbe7a46528035bb5a27
tatamibox <tat4mi@gmail.com>
2022-06-18 18:02:15 -0700

    integrate pokeAPI

--- app.js
+++ app.js
@@ -1,6 +1,35 @@
-var sound = new Howl({
-    src: ['./music/lakeTheme.webm'],
-    html5: true
-});
+const createPokemonInfo = (pokemon) => {
+    const pokemonCard = document.createElement('div');
+    pokemonCard.innerHTML = `${pokemon.id}`;
+    pokedexContainer.appendChild(pokemonCard);
+}
+const fetchPoke = async () => {
+    for (let i = 1; i <= 151; i++) {
+        await getPokemon(i);
+    }
+}
+const getPokemon = async id => {
+    const url =
+        `https://pokeapi.co/api/v2/pokemon/${id}`
+    const res = await fetch(url);
+    const pokemon = await res.json();
+    createPokemonInfo(pokemon);
+}
 
-sound.play();
+const pokedexOpenButton = document.querySelector('.openPokedex')
+const pokedexContainer = document.querySelector('.pokedex')
+const pokedexOpen = () => {
+    alert('hello!');
+}
+
+const wipeContainer = () => {
+    pokedexContainer.innerHTML = '';
+}
+
+
+const loadPokedex = () => {
+    wipeContainer();
+    fetchPoke();
+}
+
+pokedexOpenButton.onclick = loadPokedex;
